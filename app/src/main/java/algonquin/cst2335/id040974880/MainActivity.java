5d6c27cc1d14ddfdc651f8dd72454a7fb8f9bdcd
suyaliu <liu00525@algonquinlive.com>
2021-06-09 15:36:37 -0400

    week4 finished

--- app/src/main/java/algonquin/cst2335/id040974880/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/id040974880/MainActivity.java
@@ -24,122 +24,102 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-   private static String TAG ="MainActivity";
-   private static final  String FILE_NAME = "week4.txt";
+    private static String TAG = "MainActivity";
+    private static final String FILE_NAME = "week4.txt";
 
-   public static final String EMAIL = "email";
-   Button loginButton;
-   EditText editEmail;
-   TextView textView;
-   Intent callIntent;
-   Intent cameraIntent;
-   Bitmap mBitmap;
+    public static final String EMAIL = "email";
+    Button loginButton;
+    EditText editEmail;
+    TextView textView;
+    Intent callIntent;
+    Intent cameraIntent;
+    Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Log.d(TAG,"Message");
-       Log.w("MainActivity","In onCreate()-Loading Widgets" );
-        loginButton =findViewById(R.id.loginButton);
-         editEmail = findViewById(R.id.editEmail);
+          // Log.d(TAG,"Message");
+        Log.w("MainActivity", "In onCreate()-Loading Widgets");
+        loginButton = findViewById(R.id.loginButton);
+        editEmail = findViewById(R.id.editEmail);
+        //load email from sharedpreferences
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("Email", "");
+        editEmail.setText(emailAddress);
+
+        loginButton.setOnClickListener(clk -> {
 
 
-        loginButton.setOnClickListener(clk->{
+            //save to disk
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("Email", editEmail.getText().toString());
+            editor.apply();
 
-            callIntent = new Intent(MainActivity.this,SecondActivity.class);
-            callIntent.putExtra("EmailAddress",editEmail.getText().toString());
-            callIntent.putExtra("Age",40);
-            startActivityForResult(callIntent,1234);
+            callIntent = new Intent(MainActivity.this, SecondActivity.class);
+            callIntent.putExtra("EmailAddress", editEmail.getText().toString());
+            callIntent.putExtra("Age", 40);
+            startActivityForResult(callIntent, 1234);
 
 
         });
 
 
         File file = new File("FILE_NAME");
-        if(file.exists()){
+        if (file.exists()) {
             ImageView imageView = findViewById(R.id.imageView);
 
             Bitmap theImage = BitmapFactory.decodeFile(FILE_NAME);
             imageView.setImageBitmap(theImage);
         }
-   //     saveData();
-//        updateViews();
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-       Intent fromNextPage = data;
-        if(requestCode == 1234){
-            if(resultCode == 123){
-            String city = fromNextPage.getStringExtra("city");
-            int age = fromNextPage.getIntExtra("Age",0);
-            }
-        }
-
-        FileOutputStream fout = null;
-        try {
-            fout = openFileOutput(FILE_NAME,MODE_PRIVATE);
-            mBitmap.compress(Bitmap.CompressFormat.PNG,100,fout);
-            fout.flush();
-            Toast.makeText(this,"saved to "+getFilesDir()+"/"+FILE_NAME,Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            if(fout !=null){
-                try {
-                    fout.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        Intent fromNextPage = data;
+        if (requestCode == 1234) {
+            if (resultCode == 123) {
+                String city = fromNextPage.getStringExtra("city");
+                int age = fromNextPage.getIntExtra("Age", 0);
             }
         }
 
 
     }
-    public void saveData(){
-        SharedPreferences prefs = getSharedPreferences(FILE_NAME,MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(EMAIL,textView.getText().toString());
-        String  emailAddress = prefs.getString("EMAIL","");
-        textView.setText(emailAddress);
-        editor.apply();
-            }
-
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"In onStart()");
+        Log.d(TAG, "In onStart()");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"in onResume()");
+        Log.d(TAG, "in onResume()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG,"in onPause()");
+        Log.d(TAG, "in onPause()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG,"in onStop()");
+        Log.d(TAG, "in onStop()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"in onDestroy()");
+        Log.d(TAG, "in onDestroy()");
     }
 
 
--- app/src/main/java/algonquin/cst2335/id040974880/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/id040974880/SecondActivity.java
@@ -3,42 +3,63 @@ package algonquin.cst2335.id040974880;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class SecondActivity extends AppCompatActivity {
-
-
+    private static String TAG = "SecondActivity";
+    EditText inputPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         TextView topOfScreen = findViewById(R.id.textView);
+        //load phone from sharedpreferences
+        SharedPreferences prefs = getSharedPreferences("MyPhone", Context.MODE_PRIVATE);
+        int phoneNumber = prefs.getInt("PhoneNumber", 0);
+        inputPhoneNumber.setText(Integer.toString( phoneNumber ));
 
-        Intent fromPrevious=getIntent();
+
+        inputPhoneNumber = findViewById(R.id.editTextPhone);
+        Intent fromPrevious = getIntent();
         String emailAdress = fromPrevious.getStringExtra("EmailAddress");
         //int age = fromPrevious.getIntExtra("Age",0);
 
-        topOfScreen.setText("You typed " +emailAdress );
+        topOfScreen.setText("You typed " + emailAdress);
         Button btn1 = findViewById(R.id.callButton);
-        btn1.setOnClickListener(clk ->{
+        btn1.setOnClickListener(clk -> {
+
+            //save phone to disk
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("PhoneNumber", Integer.parseInt(inputPhoneNumber.getText().toString()));
+            editor.apply();
+            String callNumber = inputPhoneNumber.getText().toString();
+
             Intent call = new Intent(Intent.ACTION_DIAL);
-            call.setData(Uri.parse("tel:" +"613 302 0588"));
+            call.setData(Uri.parse("tel:" +callNumber));
             startActivity(call);
         });
 
-     Button btn_camera = findViewById(R.id.pictureButton);
-     btn_camera.setOnClickListener(clk ->{
+        Button btn_camera = findViewById(R.id.pictureButton);
+        btn_camera.setOnClickListener(clk -> {
 
-        Intent cameraPic = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-         startActivityForResult(cameraPic,3456);
+            Intent cameraPic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraPic, 3456);
 
 //         Intent databack = new Intent();
 //         databack.putExtra("Age",35);
@@ -46,22 +67,41 @@ public class SecondActivity extends AppCompatActivity {
 //         setResult(123,databack);
 //         finish();
 
-     });
+        });
 
 
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         ImageView profileImage = findViewById(R.id.imageView);
-        if(requestCode == 3456){
+        if (requestCode == 3456) {
 
-            if(resultCode ==RESULT_OK)
-            {
+            if (resultCode == RESULT_OK) {
                 Bitmap thumbnail = data.getParcelableExtra("data");
                 profileImage.setImageBitmap(thumbnail);
+
+                FileOutputStream fout = null;
+                try {
+                    fout = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                    thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fout);
+                    fout.flush();
+                    fout.close();
+                    //  Toast.makeText(this,"saved to "+getFilesDir()+"/"+FILE_NAME,Toast.LENGTH_LONG).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "in onPause()");
+    }
 }
